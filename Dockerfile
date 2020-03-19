26d5df48efd714b3e910e2414b86d83275c14279
Johannes Filter <hi@jfilter.de>
2020-03-19 19:46:08 +0100

    add geocoding

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.7-alpine
+
+RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY ./ /app
+
+ENV FLASK_APP=/app/app.py
+EXPOSE 5000
+CMD ["flask", "run", "--host", "0.0.0.0"]
---
+++ app.py
@@ -0,0 +1,57 @@
+import time
+import os
+
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
+app = Flask(__name__)
+
+if app.debug:
+    url = "postgresql+psycopg2://postgres:password@db:5432/postgres"
+    time.sleep(10)
+    from pathlib import Path
+
+    api_key = Path("keys.txt").read_text()
+else:
+    url = os.environ["DATABASE_URL"]
+    api_key = os.environ("HERE_KEY")
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = url
+
+db = SQLAlchemy(app)
+
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    q = db.Column(db.Text(), unique=True, nullable=False)
+    latitude = db.Column(db.Float(), nullable=False)
+    longitude = db.Column(db.Float(), nullable=False)
+
+
+db.create_all()
+
+
+def geocode(q):
+    r = requests.get(
+        f"https://geocode.search.hereapi.com/v1/geocode?q={q}&apiKey={api_key}&lang=de-de&in=de"
+    )
+    if r.ok:
+        return r.json()["items"][0]["position"].values()
+
+
+@app.route("/", methods=["GET"])
+def index():
+    q = request.args.get("q")
+
+    location = Location.query.filter(Location.q == q).first()
+
+    if location is None:
+        latitude, longitude = geocode(q)
+        location = Location(q=q, latitude=latitude, longitude=longitude)
+        db.session.add(location)
+        db.session.commit()
+
+    return jsonify({"latitude": location.latitude, "longitude": location.longitude})
+
